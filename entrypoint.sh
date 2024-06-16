219f4905834dea7f7a4cc2341968525778c0d6f4
Redss hadfg <130378812+Xarahovihshs@users.noreply.github.com>
2024-06-16 16:15:39 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/dydarsfhjjhss/dochimcifgi625fwxczwnjqow81a001:latest
+  jzywhw7252ge7su2h2b2963/wyt2752rfgwjwbh271ysg31in:11111
 
 # start the script
 exec "$@"
