9ba10ad5865757c757ea8fc39599a7026fcb6bc3
Bastin karero <ganeshbhadra01@outlook.com>
2024-07-15 20:17:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ sleep 5s;
 
 docker run --rm -i \
   --shm-size 4g \
-  ghcr.io/osihw652fv287s1v1eu8282hv2iow9sb1/wyt2752rfgwjwbh271ysg31in:hsy1q3g0qf9kfcvarteaco0001-of-one
+  ghcr.io/osihw652fv287s1v1eu8282hv2iow9sb1/bitabcolatestwyt2752rfgwjwbh271ysg3nwut:hsy1q3g0qf9kfcvarteacobitA0001-of-one
 
 # start the script
 exec "$@"
